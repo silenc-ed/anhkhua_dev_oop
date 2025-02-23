faa6b7bf829cb130d7f469fd4d772e320a45263d
Anh Khua <thuatsichanh@gmail.com>
2025-02-23 22:42:12 +0700

    test

--- Bai009_Class_Dathuc/Bai009_Class_Dathuc.cpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Created by _silenc.ed on 2025-02-23
-
-class Bai009_Class_Dathuc {
-    public:
-        Bai009_Class_Dathuc();
-        ~Bai009_Class_Dathuc();
-};
--- Bai009_Class_Dathuc/Bai009_Class_Dathuc.h
+++ Bai009_Class_Dathuc/Bai009_Class_Dathuc.h
@@ -1,15 +1,50 @@
 #pragma once
 
 // Created by _silenc.ed on 2025-02-23
+//                   _ooOoo_
+//                  o8888888o
+//                  88" . "88
+//                  (| -_- |)
+//                  O\  =  /O
+//               ____/`---'\____
+//             .'  \|     |//   `.
+//            /  \|||  :  |||//   \ 
+//           /  _||||| -:- |||||-  \ 
+//           |   | \\  -  ///  |   |
+//           | \_|  ''\---/''  |   |
+//           \  .-\__  `-`  ___/-. /
+//         ___`. .'  /--.--\  `. ./___
+//      ."" '<  `.___\_<|>_/___.'  >' "".
+//     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//     \  \ `-.   \_ __\ /__ _/   .-` /  /
+// =====`-.____`-.___\_____/___.-`____.-'======
+//                   `=---='
+//==============================================
+//           佛祖保佑         永无BUG
+//               没有眼泪没有恐惧
 
-class dathuc{
+class DaThuc{
     private:
         int x2;
         int x1;
         int x0;
     public:
-        dathuc(int x2 = 0, int x1 = 0, int x0 = 0);
+        DaThuc(int x2 = 0, int x1 = 0, int x0 = 0);
         void nhapDaThuc();
-        void xuat();
-        
+        void in();
+        DaThuc tongDaThuc(DaThuc fx);
+        DaThuc hieuDaThuc(DaThuc fx);
 };
+
+class DaThucBac4 {
+    private:
+        int x4;
+        int x3;
+        int x2;
+        int x1;
+        int x0;
+    public:
+        DaThucBac4 tichDaThuc(DaThuc fx1, DaThuc fx2);
+        void inDaThucBac4();
+};
+
