76f660bc6b25f99c0c7a21fbaf17838c42702d95
Anh Khua <thuatsichanh@gmail.com>
2025-03-07 13:24:30 +0700

    test

--- theme.cpp
+++ theme.cpp
@@ -1,46 +1,59 @@
-#include "Bai008_Class_Date.h"
 #include <iostream>
-
-using namespace std;
+#include <vector>
 
 int main() {
-    Date n;
+    maxtrix mt1, mt2;
+    cout << "Nhap kich thuoc ma tran 1: \n";
+    mt1.nhap();
+    cout << "Nhap kich thuoc ma tran 2: \n";
+    mt2.nhap();
 
-    while (1) {
+    while (true) {
         cout << "---------------------------- \n";
-        cout << "chon viec ban muon lam: \n";
+        cout << "Chon viec ban muon lam: \n";
         cout << "---------------------------- \n";
-        cout << "1. check nam nhuan \n";
-        cout << "2. in ngay trong thang \n";
-        cout << "3. doan ngay thu n tiep theo \n";
-        cout << "4. thoat \n";
+        cout << "1. Tinh tong 2 ma tran \n";
+        cout << "2. Tinh hieu 2 ma tran \n";
+        cout << "3. Tinh tich 2 ma tran \n";
+        cout << "4. Sua ma tran 1 \n";
+        cout << "5. Sua ma tran 2 \n";
+        cout << "6. Thoat \n";
         cout << "---------------------------- \n";
-        cout << "nhap so: ";
+        cout << "Nhap so: ";
         int x;
-
-        while (!(cin >> x) || !(x > 0 && x < 4)) {
-            cout << "nhap lai di dcmm: ";
-            cin >> x;
-        }
+        cin >> x;
 
         if (x == 1) {
             cout << "---------------------------- \n";
-        }
-
-        else if (x == 2) {
+            cout << "Ket qua: \n";
             cout << "---------------------------- \n";
-            
-        }
-        
-        else if (x == 3) {
+            maxtrix kq = mt1.tong(mt2);
+            kq.xuat();
+        } else if (x == 2) {
             cout << "---------------------------- \n";
-            
-        }
-
-        else if (x == 4) {
+            cout << "Ket qua: \n";
             cout << "---------------------------- \n";
-            cout << "chet me may di!!";
-            return 0; 
+            maxtrix kq = mt1.hieu(mt2);
+            kq.xuat();
+        } else if (x == 3) {
+            cout << "---------------------------- \n";
+            cout << "Ket qua: \n";
+            cout << "---------------------------- \n";
+            maxtrix kq = mt1.tich(mt2);
+            kq.xuat();
+        } else if (x == 4) {
+            cout << "---------------------------- \n";
+            mt1.nhap();
+        } else if (x == 5) {
+            cout << "---------------------------- \n";
+            mt2.nhap();
+        } else if (x == 6) {
+            cout << "---------------------------- \n";
+            cout << "Ket thuc!!\n";
+            return 0;
+        }        
+        else {
+            cout << "Lua chon khong hop le, vui long thu lai!\n";
         }
     }
 }
